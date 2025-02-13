a87397b10c8c056e46cc9f538bc1373ce9286138
Josh Endemann <jahend44@gmail.com>
2025-02-12 19:06:29 -0600

    Response Helpers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+// use Illuminate\Http\Response; 
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,4 +26,23 @@ Route::get('/test', function(Request $request) {
 
 Route::get('/users', function(Request $request) {
     return $request->except(['name']);
+});
+
+Route::get('/test', function() {
+    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');
+});
+
+
+Route::get('/download', function() {
+    return response()->download(public_path('favicon.ico'));
+});
+
+Route::get('/error', function() {
+    return response('Not Found', 404);
+});
+
+
+Route::get('/read-cookie', function() {
+    $cookieValue = $request->cookie('name');
+    return response()->json(['cookie'=> $cookieValue]);
 });
