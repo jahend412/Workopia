0b268fa47c85d789da523a86d00ddd4ead3657b6
Josh Endemann <jahend44@gmail.com>
2025-02-12 18:35:42 -0600

    Intro to routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return 'Available Jobs';
+})->name('jobs');
+
+Route::any('/submit', function () {
+    return 'Submitted';
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href='$url'>Click Here</a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'john@gmail.com'
+    ];
+});
+
+
+
