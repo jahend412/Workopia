1db6c4b1fed21f57d6c07d6e1947e708cfac8b13
Josh Endemann <jahend44@gmail.com>
2025-02-12 18:57:18 -0600

    request helper

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,14 +11,18 @@ Route::get('/jobs', function () {
     return 'Available Jobs';
 })->name('jobs');
 
-Route::any('/submit', function () {
-    return 'Submitted';
+Route::get('/test', function(Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'method' => $request->method(),
+    ];
 });
 
-Route::get('/post/{id}', function (string $id) {
-    return 'Post ' . $id;
-})->whereNumber('id');
-
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . 'Comment ' . $commentId;
+Route::get('/users', function(Request $request) {
+    return $request->except(['name']);
 });
