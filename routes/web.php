fbb4f62ce8e2dca250068e45dd7701b086f1fde4
Josh Endemann <jahend44@gmail.com>
2025-02-13 19:44:22 -0600

    Create and display Views in Laravel
    Created a jobs.php and create.php file with basic html and then created the routes in the web.php file using proper syntax.

--- routes/web.php
+++ routes/web.php
@@ -9,40 +9,10 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return 'Available Jobs';
+    return 'jobs.index';
 })->name('jobs');
 
-Route::get('/test', function(Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'method' => $request->method(),
-    ];
-});
+Route::get('/jobs/create', function () {
+    return 'jobs.create';
+})->name('jobs.create');
 
-Route::get('/users', function(Request $request) {
-    return $request->except(['name']);
-});
-
-Route::get('/test', function() {
-    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');
-});
-
-
-Route::get('/download', function() {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/error', function() {
-    return response('Not Found', 404);
-});
-
-
-Route::get('/read-cookie', function() {
-    $cookieValue = $request->cookie('name');
-    return response()->json(['cookie'=> $cookieValue]);
-});
